e19e4b860c4236dcbd429f258120c0e358a7878c
Aurora-20-10 <ngocmai20102003@gmail.com>
2025-07-23 19:48:20 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -259,6 +259,7 @@ function populateClusterSelect() {
 // === KHỞI TẠO ===
 document.addEventListener("DOMContentLoaded", () => {
   renderSkills();
+  buildClusterDashboard();
   populateClusterSelect();
   populateClusterFilter();
 });
